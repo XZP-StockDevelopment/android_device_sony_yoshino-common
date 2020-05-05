c0a6c51e3878d39a6ebbf77c33bb8d61dc94f9c4
mfshujath <mmfshujath@gmail.com>
2020-05-05 21:44:26 +0000

    AOSP Bringup

---
+++ hardware/modem/Android.mk
@@ -0,0 +1,67 @@
+ifeq ($(BOARD_MODEM_CUSTOMIZATIONS),true)
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+CUSTOMIZATION_MODEM_SYMLINKS := $(TARGET_OUT)/etc/customization/modem
+$(CUSTOMIZATION_MODEM_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "Create customization modem links: $@"
+	@mkdir -p $@
+	$(hide) ln -sf amss_fs_empty.mbn $@/reset_modemst1
+	$(hide) ln -sf amss_fs_empty.mbn $@/reset_modemst2
+ifeq ($(TARGET_DEVICE),lilac)
+	$(hide) ln -sf amss_fsg_lilac_tar.mbn $@/default
+endif
+ifeq ($(TARGET_DEVICE),poplar)
+	$(hide) ln -sf amss_fsg_poplar_tar.mbn $@/default
+endif
+ifeq ($(TARGET_DEVICE),poplar_canada)
+	$(hide) ln -sf amss_fsg_poplar_tar.mbn $@/default
+endif
+ifeq ($(TARGET_DEVICE),poplar_dsds)
+	$(hide) ln -sf amss_fsg_poplar_dsds_tar.mbn $@/default
+endif
+ifeq ($(TARGET_DEVICE),maple)
+	$(hide) ln -sf amss_fsg_maple_tar.mbn $@/default
+endif
+ifeq ($(TARGET_DEVICE),maple_dsds)
+	$(hide) ln -sf amss_fsg_maple_dsds_tar.mbn $@/default
+endif
+
+ALL_DEFAULT_INSTALLED_MODULES += \
+	$(CUSTOMIZATION_MODEM_SYMLINKS)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := init.sony.modem.sh
+LOCAL_SRC_FILES := init.sony.modem.sh
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_STEM := init.sony.modem.sh
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_EXECUTABLES)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := dump_miscta
+LOCAL_SRC_FILES := dump_miscta.c
+LOCAL_SHARED_LIBRARIES := libmiscta
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_EXECUTABLES)
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := ta_cust_version
+LOCAL_SRC_FILES := ta_cust_version.c
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_EXECUTABLES)
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+# Create mount points for oem configs
+OEM_MOUNT_POINTS := $(TARGET_ROOT_OUT)/oem
+
+ALL_DEFAULT_INSTALLED_MODULES += $(OEM_MOUNT_POINTS)
+
+$(OEM_MOUNT_POINTS):
+	@echo "Creating $(OEM_MOUNT_POINTS) subdirs"
+	@mkdir -p $(TARGET_ROOT_OUT)/oem/modem-config
+	@mkdir -p $(TARGET_ROOT_OUT)/oem/system-properties
+
+endif
---
+++ platform/hardware.mk
@@ -0,0 +1,44 @@
+### GRAPHICS
+PRODUCT_PACKAGES += \
+    copybit.msm8998 \
+    gralloc.msm8998 \
+    hwcomposer.msm8998 \
+    memtrack.msm8998 \
+    libdisplayconfig \
+    liboverlay
+
+### AUDIO
+PRODUCT_PACKAGES += \
+    libvolumelistener \
+    bthost_if
+
+# IPACM
+PRODUCT_PACKAGES += \
+    ipacm \
+    IPACM_cfg.xml \
+    libipanat \
+    liboffloadhal
+
+# DISPLAY
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm
+
+# MODEM
+PRODUCT_PACKAGES += \
+    init.sony.modem.sh
+
+# SENSORS
+PRODUCT_PACKAGES += \
+    libsensorndkbridge
+
+# THERMAL
+PRODUCT_PACKAGES += \
+    thermal.msm8998
+
+# VR
+PRODUCT_PACKAGES += \
+    vr.msm8998
+
+# COVER
+PRODUCT_PACKAGES += \
+    FlipFlap
