e32ff346e3d8e962ae66394e6a2e58332f820b30
Shashank Verma <shashank.verma2002@gmail.com>
2020-05-20 16:39:47 +0800

    Modem and TA tweaks

--- platform/dependencies.mk
+++ platform/dependencies.mk
@@ -10,3 +10,14 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
 	android.hardware.secure_element@1.0 \
 	android.hardware.secure_element@1.1
+
+## Include MODEM (yoshino-common/hardware/modem)
+include device/sony/yoshino-common/hardware/modem/Android.mk
+
+PRODUCT_PACKAGES += \
+	dump_miscta \
+	ta_cust_version
+
+PRODUCT_PROPERTY_OVERRIDES += \
+	persist.vendor.radio.calls.on.ims=1 \
+	persist.radio.calls.on.ims=1
