3e823c4b727d27890090d8245af64f9a391bf8bb
Shashank Verma <shashank.verma2002@gmail.com>
2020-05-20 09:09:58 +0800

    IMS & SecureElement tweaks
    05-19 22:07:25.448  4313  4313 F DEBUG   : pid: 813, tid: 813, name: secure_element@  >>> /vendor/bin/hw/android.hardware.secure_element@1.0-service <<<
    05-19 22:07:25.448  4313  4313 F DEBUG   : uid: 1068
    05-19 22:07:25.448  4313  4313 F DEBUG   : signal 6 (SIGABRT), code -1 (SI_QUEUE), fault addr --------
    05-19 22:07:25.448  4313  4313 F DEBUG   : Abort message: 'Check failed: config_path != "" '

--- platform/config-files.mk
+++ platform/config-files.mk
@@ -83,7 +83,8 @@ PRODUCT_COPY_FILES += \
 
 # IMS
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.ims.xml
+    frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.ims.xml \
+	frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml
 
 ### WIFIDISPLAY
 PRODUCT_COPY_FILES += \
--- platform/dependencies.mk
+++ platform/dependencies.mk
@@ -5,3 +5,8 @@ PRODUCT_PACKAGES += \
     libmmlib2d_interface \
     libmm-qcamera \
     libqomx_core
+
+## Extra SecureElement interface
+PRODUCT_PACKAGES += \
+	android.hardware.secure_element@1.0 \
+	android.hardware.secure_element@1.1
